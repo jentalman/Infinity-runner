4b4b954cebfd13e69da1e48a5e9c90489dd5a036
Jentalman <viktorjentalman@gmail.com>
2021-03-03 13:45:59 +0300

    init commit

---
+++ Assets/Scripts/PlayerScripts/Mover.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Mover : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    [SerializeField] private Rigidbody2D _rigidbody2D;
+
+    private void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+}
