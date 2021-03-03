6a0c808b86448863bdea7b267255d98577b3437a
Jentalman <viktorjentalman@gmail.com>
2021-03-03 15:41:35 +0300

    Add Player control

---
+++ Assets/Scripts/PlayerScripts/PlayerInput.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+
+    private float _moveHorizontal;
+    private float _moveVertical;
+
+ 
+    public void PlayerInputMovement(out Vector2 movement)
+    {
+        _moveHorizontal = Input.GetAxis("Horizontal");
+        _moveVertical = Input.GetAxis("Vertical");
+        movement = new Vector2(_moveHorizontal, _moveVertical);
+    }
+}
---
+++ Assets/Scripts/PlayerScripts/PlayerMover.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(PlayerInput))]
+public class PlayerMover : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+
+    private Rigidbody2D _rigidbody2D;
+    private PlayerInput _playerInput;
+    private Vector2 movement;
+
+    private void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        _playerInput = GetComponent<PlayerInput>();
+    }
+    private void Update()
+    {
+        _playerInput.PlayerInputMovement(out movement);
+        PlayerMove(movement);
+    }
+
+    private void PlayerMove(Vector2 movement)
+    {
+        _rigidbody2D.AddForce(movement * _speed * Time.deltaTime, ForceMode2D.Force);
+        _rigidbody2D.velocity = Vector2.zero;
+    }
+}
